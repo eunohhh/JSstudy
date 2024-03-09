59658e9e23499d76a47df230b0080a136cd71241
Eun <chorockmasil@gmail.com>
2024-03-10 03:03:28 +0900

    240310

--- solutions.js
+++ solutions.js
@@ -28,3 +28,72 @@ function fractionSum(numer1, denom1, numer2, denom2) {
     
     return answer;
 }
+
+const forManyValue = [1,1,2,2];
+
+const res = manyValue(forManyValue);
+
+function manyValue(array) {
+    let answer = 0;
+
+    const arr = [];
+    const countedNumbers = array.reduce(function (allNumbers, number) {
+        if (number in allNumbers) {
+            allNumbers[number]++;
+        } else {
+            allNumbers[number] = 1;
+        }
+        return allNumbers;
+    }, {});
+
+    const max = Math.max(...Object.values(countedNumbers));
+
+    for(const [key, value] of Object.entries(countedNumbers)){
+        if(value === max){
+            arr.push(Number(key));
+        }
+    }
+
+    if(arr.length === 1){
+        answer = arr[0];
+    }else if(arr.length > 1){
+        answer = -1;
+    }
+
+    // if(array.length === 1) answer = array[0];
+
+    // const arr = [];
+    // const values = [];
+    // const set = [...new Set(array)]
+
+    // const result = arr.filter((e,i)=>{
+
+    //     return arr[`${e}`] === max
+    // })
+
+
+    // for(let i = 0; i < set.length; i++){
+    //     for(let j = 0; j < array.length; j++){
+    //         if(set[i] === array[j]){
+    //             arr.push({
+    //                 val : set[i]
+    //             })
+    //         }
+    //     }
+    // }
+    // const filtered = array.filter((e,i) =>{
+    //     return array.indexOf(e) !== i
+    // })
+
+    // const set = [...new Set(filtered)];
+    
+    // if(set.length === 0){
+    //     answer = array[0];
+    // }else if(set.length === 1){
+    //     answer = set[0];
+    // }else if(set.length > 1){
+    //     answer = -1;
+    // }
+            
+    return answer;
+}
